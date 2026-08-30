6ec0fd6c6709d7a1da7d48de13525272c70c89ba
Deep <deepdetroja1223@gmail.com>
2026-08-30 10:40:50 +0530

    initialize Frontend and create all relevent Packages to start the project

--- apps/backend/src/Routes/auth.router.ts
+++ apps/backend/src/Routes/auth.router.ts
@@ -1,14 +1,21 @@
 import {Router} from "express";
-import {signup,signin,me,profile} from "../controllers/auth.controller.js"
 import { middleware } from "../middleware/auth.js";
 
 
 const router = Router();
 
-router.post("/signup", signup);
-router.post("/signin", signin);
-router.get("/me", middleware, me);
-router.post("/profile", middleware, profile);
+router.post("/signup", (req,res)=>{
+    //signup endpoint
+});
+router.post("/signin", (req,res)=>{
+    //signin endpoint
+});
+router.get("/me", middleware, (req,res)=>{
+    //me endpoint
+});
+router.post("/profile", middleware, (req,res)=>{
+    //profile endpoint
+});
 
 
 export const authRouter:Router = router;
--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { PORT } from "./config/env.js";
+import { authRouter } from "./Routes/auth.router.js";
 
 const app =express();
 
